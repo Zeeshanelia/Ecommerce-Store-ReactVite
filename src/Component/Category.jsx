6f14270e3c4e32a8806231d802ae45043f6c6056
cholexMax <tobexnewlife@gmail.com>
2025-01-02 21:09:25 +0530

    updated layoutall.jsx and category.jsx to resolve issue

--- src/Component/Category.jsx
+++ src/Component/Category.jsx
@@ -28,21 +28,10 @@ const Category = () => {
             title: 'Electronics'
         }
     ]);
-
-    console.log(category)
     return (
-        <LayoutAll>
-            <div className='md:p-16 p-8'>
-                <div className='md:w-10/12 mx-auto grid md:grid-cols-4 md:gap-16 gap-8'>
-                    {category.map((item, index) => (
-                        <div key={index} className='hover:bg-orange-600 hover:text-white border rounded-lg bg-white shadow-lg flex flex-col p-8 justify-center items-center'>
-                            <i className="ri-menu-search-line text-6xl"></i>
-                            <h1 className='text-2xl font-bold'>{item.title}</h1>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </LayoutAll>
+        <>
+        <LayoutAll Category={category}/>
+        </>
     );
 };
 
